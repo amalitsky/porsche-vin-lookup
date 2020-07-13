8c5e16e82c54075c57bd0ccb51222b4c2a832b40
Alex Malitsky <1457274+amalitsky@users.noreply.github.com>
2020-07-12 22:39:01 -0700

    chor(script): move options to separate file

--- src/lookup.py
+++ src/lookup.py
@@ -1,6 +1,7 @@
 import requests
 import sys
 from bs4 import BeautifulSoup
+from options import importantOptionCodesSet
 
 def getRowToPrint(value, label = '', isBold = False):
     str = ''
@@ -19,7 +20,7 @@ def getRowToPrint(value, label = '', isBold = False):
     return str
 
 if (len(sys.argv) != 2):
-    raise Exception('most likely VIN is not passed')
+    raise Exception('most likely VIN was not passed')
 
 # seemingly useless noise
 ignoredValues = [
@@ -29,38 +30,6 @@ ignoredValues = [
 ]
 
 ignoredValuesSet = set(ignoredValues)
-
-importantOptionCodes = [
-    '9VL', # bose
-    '9WT', # apple car play
-    '030', # PASM 20mm
-    '220', # PTV (prev gen GTS)
-    '250', # PDK = no go
-    '397', # 19" Boxster S Wheels
-    '398', # 19" Cayman S Wheels
-    '475', # Porsche Active Suspension Management (PASM) (Lowered 10 mm)
-    '480', # manual
-    '489', # heated multif steering
-    '602', # LED headlights
-    '603', # bi xenon
-    '625', # entry & drive
-    '680', # bose (prev gen)
-    '858', # gt sport steering wheel
-    '9VJ', # Burmester
-    'A1', # black - no go
-    'G1', # guards red
-    'H2', # lava orange
-    'J5', # maimi blue
-    'P3', # racing yellow
-    'P04', # Sport Seats Plus (2-way)
-    'P07', # sport seat plus 18-way
-    'QR5', # sport chrono
-    'XEW', # Bi-Xenon Headlights in Black with Porsche Dynamic Light System
-    'XLF', # sport exhaust 2016
-    'XLX', # sport exhaust
-]
-
-importantOptionCodesSet = sorted(set(importantOptionCodes))
 
 # top report section
 headerProps = [
